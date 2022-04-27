443953cde8d5bcdcc11e35c2fda29fe47f7350e3
«Igor <talovez@yandex.ru>
2022-04-27 19:14:10 +0300

    initial

---
+++ Task 54/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+void PrintArray(int[,] array) // метод печать массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] array) // метод заполнение массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+Console.WriteLine("Введите количество строк в заданном массиве");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в заданном массиве");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] arrayinput = new int[m,n];
+FillArray(arrayinput);
+Console.WriteLine("Входной массив:");
+PrintArray(arrayinput);
+int[,] arraysort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            int tempj = j;
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > array[i, tempj]) tempj = k;
+            }
+            int selection = array[i, j];
+            array[i, j] = array[i, tempj];
+            array[i, tempj] = selection;
+        }
+    }
+    return array;
+}
+Console.WriteLine("Сортировка строк по убыванию:");
+PrintArray(arraysort(arrayinput));
---
+++ Task 56/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив.
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Console.WriteLine("Введите количество строк в заданном массиве");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в заданном массиве");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m,n];
+// заполнение массива и печать
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+    array[i, j] = new Random().Next(0,11);
+    Console.Write(array[i,j]+" ");
+    }
+}
+// подсчёт сумм элементов построчно
+Console.WriteLine();
+int minsummline = 1;
+int summline = 0;
+ for (int j = 0; j < array.GetLength(1); j++)
+        {
+            summline = summline + array[0,j];
+        }
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        int summ = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            summ = summ + array[i,j];
+        }
+        if (summ < summline) 
+        {
+            summline = summ;
+            minsummline = i+1;
+        }
+    }
+Console.WriteLine($"Строка с наименьшей суммой элементов - {minsummline}");
